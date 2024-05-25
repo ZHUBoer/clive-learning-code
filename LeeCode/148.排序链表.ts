a3f90e0ebf27d3688391edd07fa9f16811ad0927
ZHUBoer <1543865065@qq.com>
2024-05-26 01:12:09 +0800

    feat: 增加 leetcode配置

---
+++ LeeCode/148.排序链表.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=148 lang=typescript
+ *
+ * [148] 排序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function sortList(head: ListNode | null): ListNode | null {
+    
+};
+// @lc code=end
+
---
+++ LeeCode/2.两数相加.ts
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=typescript
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  return null;
+}
+
+console.log(addTwoNumbers(null, null));
+// @lc code=end
