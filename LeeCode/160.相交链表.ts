8a8d74a051ee246ef2b8e9fcdf99a49469c9a4ba
clivezhu <clivezhu@tencent.com>
2024-05-30 17:27:41 +0800

    chore

--- LeeCode/160.相交链表.ts
+++ LeeCode/160.相交链表.ts
@@ -13,37 +13,33 @@ class ListNode {
   next: ListNode | null;
 
   constructor(val?: number, next?: ListNode | null) {
-      this.val = (val === undefined ? 0 : val);
-      this.next = (next === undefined ? null : next);
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
   }
 }
 
-function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
-
+function getIntersectionNode(
+  headA: ListNode | null,
+  headB: ListNode | null
+): ListNode | null {
   // map 解法
-  let mapA = new Map();
-  let urA = headA;
-  while (urA) {
-    mapA.set(urA, true);
-    urA = urA.next;
+  let mapA = new Map<ListNode, boolean>();
+  let curA = headA;
+  while (curA) {
+    mapA.set(curA, true);
+    curA = curA.next;
   }
 
-  let mapB = new Map();
-  let urB = headB;
-  while (urB) {
-    mapB.set(urB, true);
-    urB = urB.next;
-  }
-
-  for(let [key, value] of mapA) {
-    if(mapB.has(key)) {
-      return key;
+  let curB = headB;
+  while (curB) {
+    if (mapA.has(curB)) {
+      return curB;
     }
+    curB = curB.next;
   }
+
   return null;
 
   // 相同长度解法 min+max即为共同长度，相交节点必然同时到达。每步比对即可得到结果。
-  
-};
+}
 // @lc code=end
-
