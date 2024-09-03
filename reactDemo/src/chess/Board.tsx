5bf14a9daf2da916511bdcb11c3bd758b1efb772
clivezhu <clivezhu@tencent.com>
2024-09-03 18:10:24 +0800

    chore

--- reactDemo/src/chess/Board.tsx
+++ reactDemo/src/chess/Board.tsx
@@ -29,35 +29,30 @@ export class Board extends React.Component<BoardProps, BoardState> {
     };
   }
 
-  handleClick = (curValue) => {
+  handleClick = (curValue: any) => {
     console.log(curValue);
   };
 
   renderSquare(i) {
-    return (
-      <Square
-        squareContent={i}
-        onFillSquare={() => this.handleClick(i)}
-      />
-    );
+    return <Square value={i} onSquareClick={() => this.handleClick(i)} />;
   }
 
   render() {
     const status = 'Next player: X';
     return (
       <div>
-        <div className='status'>{status}</div>
-        <div className='board-row'>
+        <div className="status">{status}</div>
+        <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className='board-row'>
+        <div className="board-row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div className='board-row'>
+        <div className="board-row">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
@@ -66,3 +61,5 @@ export class Board extends React.Component<BoardProps, BoardState> {
     );
   }
 }
+
+export function calculateWinner(squares: Squares) {}
--- reactDemo/src/chess/Square.tsx
+++ reactDemo/src/chess/Square.tsx
@@ -4,36 +4,15 @@
 import React from 'react';
 
 export interface SquareProps {
-  squareContent: string | null;
-  onFillSquare?: () => void;
-}
-
-interface SquareState {
   value: string | null;
+  onSquareClick?: () => void;
 }
 
-export class Square extends React.Component<SquareProps, SquareState> {
-  constructor(props: SquareProps) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  handleClick = () => {
-    this.setState({
-      value: 'X',
-    });
-    this.props.onFillSquare && this.props.onFillSquare();
-  };
-
-  render() {
-    return (
-      <button
-        className='square'
-        onClick={this.handleClick}
-      >
-        {this.state.value}
-      </button>
-    );
-  }
+export function Square(props: SquareProps) {
+  const { value, onSquareClick } = props;
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
