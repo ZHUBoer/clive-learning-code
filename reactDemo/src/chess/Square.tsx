21826d52580ead946e184aea958885b2cf5279a9
clivezhu <clivezhu@tencent.com>
2024-09-04 14:19:15 +0800

    flutter-test init

--- reactDemo/src/chess/Square.tsx
+++ reactDemo/src/chess/Square.tsx
@@ -3,13 +3,18 @@
  */
 import React from 'react';
 
+// 定义棋盘格子组件的属性接口
 export interface SquareProps {
-  value: string | null;
-  onSquareClick?: () => void;
+  value: string | null; // 格子的值，可以是字符串或null
+  onSquareClick?: () => void; // 点击格子时触发的可选回调函数
 }
 
+// 棋盘格子组件
 export function Square(props: SquareProps) {
+  // 解构props，获取value和onSquareClick
   const { value, onSquareClick } = props;
+
+  // 渲染一个按钮作为棋盘格子
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
