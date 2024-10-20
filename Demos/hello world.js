dd252537312fd3e0523b730bf882371ee890b7ab
ZHUBoer <1543865065@qq.com>
2024-10-20 21:38:02 +0800

    chore

--- Demos/hello world.js
+++ Demos/hello world.js
@@ -1,14 +1,14 @@
 // Node.js是CommonJS的写法，使用require来引入模块。
-var http = require("http");
+var http = require('http');
 http
   .createServer(function (request, response) {
     // 发送 HTTP头部
     // HTTP 状态值：200 :OK
     // 内容类型：text/plain
-    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
     // 发送响应数据 “Hello World”
-    response.end("Hello World\n");
+    response.end('Hello World\n');
   })
   .listen(8888);
 // 终端打印
-console.log("Server running at http://127.0.0.1:8888/");
+console.log('Server running at http://127.0.0.1:8888/');
