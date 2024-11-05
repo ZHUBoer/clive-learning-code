ae79fac0ce55c67ea9408f7ce6234efccd4aca47
clivezhu <clivezhu@tencent.com>
2024-11-05 18:10:33 +0800

    算法

---
+++ LeeCode/70.爬楼梯.ts
@@ -0,0 +1,68 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=typescript
+ *
+ * [70] 爬楼梯
+ *
+ * https://leetcode.cn/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (54.58%)
+ * Likes:    3646
+ * Dislikes: 0
+ * Total Accepted:    1.6M
+ * Total Submissions: 3M
+ * Testcase Example:  '2'
+ *
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：n = 2
+ * 输出：2
+ * 解释：有两种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶
+ * 2. 2 阶
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：n = 3
+ * 输出：3
+ * 解释：有三种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶 + 1 阶
+ * 2. 1 阶 + 2 阶
+ * 3. 2 阶 + 1 阶
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= n <= 45
+ * 
+ * 
+ */
+
+// @lc code=start
+function climbStairs(n: number): number {
+  /* 
+  * 动态规划：将问题拆分成子问题
+  * climbStairs(n) = climbStairs(n - 1) + climbStairs(n - 2)
+  * climbStars(0) = 1
+  * climbStairs(1) = 1
+  * climbStairs(2) = 2
+  */
+  const dp = [1, 1];
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+};
+// @lc code=end
+
