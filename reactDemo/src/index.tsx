d84183caa22eba8341f27d3603741fbd62e5229e
clivezhu <clivezhu@tencent.com>
2024-12-31 17:15:04 +0800

    chore

--- reactDemo/src/index.tsx
+++ reactDemo/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'https://github.com/ZHUBoer/clive-learning-code/blob/main/reactDemo/src/style/theme-pack.css';
+import 'https://github.com/ZHUBoer/clive-learning-code/blob/main/reactDemo/src/style/global.scss';
 import { Game } from './chess/Game';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
