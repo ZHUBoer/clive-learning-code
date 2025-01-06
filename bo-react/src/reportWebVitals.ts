9dd1f44ba14aaa10e87a76d1f009636efd409f45
clivezhu <clivezhu@tencent.com>
2025-01-06 16:33:44 +0800

    feat: init bo-react

---
+++ bo-react/src/reportWebVitals.ts
@@ -0,0 +1,17 @@
+// @ts-ignore
+import { ReportHandler } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    // @ts-ignore
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
---
+++ bo-react/typing.d.ts
@@ -0,0 +1,5 @@
+export { };
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
